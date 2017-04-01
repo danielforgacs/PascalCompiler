72dc4ab2ff53874a66006aacf2890d316ebbdb5a
ford @P27K-virtualBox <forgacs.daniel@gmail.com>
2017-04-01 21:17:47 +0100

    updated tests. PASSED.

--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -11,17 +11,53 @@ sys.path.append(
 import source.interpreter as itpr
 
 
+
 @pytest.mark.parametrize('userinput, result', [
     ['{0}+{1}'.format(k, i), k+i] for k in range(10) for i in range(10)
 ])
 def test_interpreter_adds_singledigit_integers_no_whitespace(userinput, result):
     assert result == itpr.Interpreter(userinput).expr()
 
+
+
 @pytest.mark.parametrize('userinput, result', [
     ['{0} + {1}'.format(k, i), k+i] for k in range(10) for i in range(10)
 ])
 def test_interpreter_adds_singledigit_integers_with_whitespace(userinput, result):
     assert result == itpr.Interpreter(userinput).expr()
+
+
+
+@pytest.mark.parametrize('userinput, result', [
+    ['{0}   +     {1}'.format(k, i), k+i] for k in range(10) for i in range(10)
+])
+def test_interpreter_adds_singledigit_integers_with_whitespace_2(userinput, result):
+    assert result == itpr.Interpreter(userinput).expr()
+
+
+
+@pytest.mark.parametrize('userinput, result', [
+    ['{0}+{1}'.format(k, i), k+i] for k in range(90, 100) for i in range(90, 100)
+])
+def test_interpreter_adds_multidigit_integers_no_whitespace(userinput, result):
+    assert result == itpr.Interpreter(userinput).expr()
+
+
+
+@pytest.mark.parametrize('userinput, result', [
+    ['{0} + {1}'.format(k, i), k+i] for k in range(90, 100) for i in range(90, 100)
+])
+def test_interpreter_adds_multidigit_integers_with_whitespace(userinput, result):
+    assert result == itpr.Interpreter(userinput).expr()
+
+
+
+@pytest.mark.parametrize('userinput, result', [
+    ['{0}   +   {1}'.format(k, i), k+i] for k in range(90, 100) for i in range(90, 100)
+])
+def test_interpreter_adds_multidigit_integers_with_whitespace_2(userinput, result):
+    assert result == itpr.Interpreter(userinput).expr()
+
 
 
 if __name__ == '__main__':

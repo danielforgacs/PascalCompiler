faef2d955700be316c1714832f5230900c8a9753
Danielf @P27K <forgacs.daniel@gmail.com>
2018-06-09 15:17:11 +0100

    added python version test to tests.

--- interpreter_test.py
+++ interpreter_test.py
@@ -1,5 +1,15 @@
 import interpreter
 import pytest
+import sys
+
+
+PYTHON_VERIONS_MAJOR = (3,)
+PYTHON_VERIONS_MINOR = (5,)
+
+
+def test_python_version_ok():
+    assert sys.version_info.major in PYTHON_VERIONS_MAJOR
+    assert sys.version_info.minor in PYTHON_VERIONS_MINOR
 
 
 def test_get_next_token_returns_token_01():

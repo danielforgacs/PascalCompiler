3857774fdaa55f32f524d39bc6a51a13f6f63e20
Danielf @P27K <forgacs.daniel@gmail.com>
2018-06-09 19:52:05 +0100

    copied code from tutor

--- interpreter.py
+++ interpreter.py
@@ -1,113 +1,134 @@
-INTEGER = 'INTEGER'
-PLUS = 'PLUS'
-MINUS = 'MINUS'
-MULT = 'MULT'
-DIV = 'DIV'
-EOF = 'EOF'
+# Token types
+#
+# EOF (end-of-file) token is used to indicate that
+# there is no more input left for lexical analysis
+INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
 
 
-class Token:
-    def __init__(self, type_, value):
-        self.type_ = type_
+class Token(object):
+    def __init__(self, type, value):
+        # token type: INTEGER, MUL, DIV, or EOF
+        self.type = type
+        # token value: non-negative integer value, '*', '/', or None
         self.value = value
 
+    def __str__(self):
+        """String representation of the class instance.
 
-class Lexer:
+        Examples:
+            Token(INTEGER, 3)
+            Token(MUL, '*')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lexer(object):
     def __init__(self, text):
-        self.text =  text
+        # client string input, e.g. "3 * 5", "12 / 3 * 4", etc
+        self.text = text
+        # self.pos is an index into self.text
         self.pos = 0
-        # self.currenttoken = None
-        self.currentchar = self.text[self.pos]
+        self.current_char = self.text[self.pos]
 
-    # def error(self):
-    #     raise Exception('::: error parsing :::')
+    def error(self):
+        raise Exception('Invalid character')
 
     def advance(self):
+        """Advance the `pos` pointer and set the `current_char` variable."""
         self.pos += 1
-        if self.pos > len(self.text)-1:
-            self.currentchar = None
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
         else:
-            self.currentchar = self.text[self.pos]
+            self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
-        while self.currentchar and self.currentchar.isspace():
+        while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
     def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
         result = ''
-        while self.currentchar and self.currentchar.isdigit():
-            result += self.currentchar
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
             self.advance()
         return int(result)
 
     def get_next_token(self):
-        while self.currentchar:
-            if self.currentchar.isspace():
+        """Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        """
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
 
-            if self.currentchar.isdigit():
-                return Token(type_=INTEGER, value=self.integer())
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
 
-            if self.currentchar == '+':
+            if self.current_char == '*':
                 self.advance()
-                return Token(type_=PLUS, value='+')
+                return Token(MUL, '*')
 
-            if self.currentchar == '-':
+            if self.current_char == '/':
                 self.advance()
-                return Token(type_=MINUS, value='-')
+                return Token(DIV, '/')
 
-            if self.currentchar == '*':
-                self.advance()
-                return Token(type_=MULT, value='*')
+            self.error()
 
-            if self.currentchar == '/':
-                self.advance()
-                return Token(type_=DIV, value='/')
-
-            # self.error()
-            raise Exception('GET NEXT TOKEN ERROR!')
-
-        return Token(type_=EOF, value=None)
+        return Token(EOF, None)
 
 
-
-class Interpreter:
+class Interpreter(object):
     def __init__(self, lexer):
         self.lexer = lexer
-        self.currenttoken = self.lexer.get_next_token()
+        # set current token to the first token taken from the input
+        self.current_token = self.lexer.get_next_token()
 
+    def error(self):
+        raise Exception('Invalid syntax')
 
-    def eat(self, tokentype):
-        if self.currenttoken.type_ != tokentype:
-            # self.error()
-            raise Exception('EAT ERROR!')
-        self.currenttoken = self.lexer.get_next_token()
-
+    def eat(self, token_type):
+        # compare the current token type with the passed token
+        # type and if they match then "eat" the current token
+        # and assign the next token to the self.current_token,
+        # otherwise raise an exception.
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
 
     def factor(self):
-        token = self.currenttoken
+        """Return an INTEGER token value.
+
+        factor : INTEGER
+        """
+        token = self.current_token
         self.eat(INTEGER)
         return token.value
 
-
     def expr(self):
-        self.currenttoken = self.lexer.get_next_token()
+        """Arithmetic expression parser / interpreter.
+
+        expr   : factor ((MUL | DIV) factor)*
+        factor : INTEGER
+        """
         result = self.factor()
 
-        while self.currenttoken.type_ in (PLUS, MINUS, MULT, DIV):
-            token = self.currenttoken
-
-            if token.type_ == PLUS:
-                self.eat(PLUS)
-                result = result + self.factor()
-            elif token.type_ == MINUS:
-                self.eat(MINUS)
-                result = result - self.factor()
-            elif token.type_ == MULT:
-                self.eat(MULT)
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
                 result = result * self.factor()
-            elif token.type_ == DIV:
+            elif token.type == DIV:
                 self.eat(DIV)
                 result = result / self.factor()
 
@@ -117,19 +138,18 @@ class Interpreter:
 def main():
     while True:
         try:
+            # To run under Python3 replace 'raw_input' call
+            # with 'input'
             text = input('calc> ')
         except EOFError:
             break
         if not text:
             break
-
-        lexer = Lexer(text=text)
-        interpreter = Interpreter(lexer=lexer)
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
 
 
-
 if __name__ == '__main__':
-    pass
     main()

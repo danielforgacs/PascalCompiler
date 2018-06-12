b5bf728c9528beb61b5e94938269d5b0819afa25
ford @mini <forgacs.daniel@gmail.com>
2018-06-12 23:18:06 +0100

    clean up

--- interpreter.py
+++ interpreter.py
@@ -12,22 +12,6 @@ class Token(object):
         self.type = type
         self.value = value
 
-    def __str__(self):
-        """String representation of the class instance.
-
-        Examples:
-            Token(INTEGER, 3)
-            Token(PLUS, '+')
-            Token(MUL, '*')
-        """
-        return 'Token({type}, {value})'.format(
-            type=self.type,
-            value=repr(self.value)
-        )
-
-    def __repr__(self):
-        return self.__str__()
-
 
 class Lexer(object):
     def __init__(self, text):

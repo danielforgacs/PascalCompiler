ac3cb2595346c245081222a1cfb51760880031ff
ford @mini <forgacs.daniel@gmail.com>
2018-06-12 23:18:16 +0100

    clean up

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,3 @@
-# Token types
-#
-# EOF (end-of-file) token is used to indicate that
-# there is no more input left for lexical analysis
 INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
     'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
 )

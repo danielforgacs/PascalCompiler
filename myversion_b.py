5e742dbeec6229b08677ab99832d9dde88ba9088
ford @mini <forgacs.daniel@gmail.com>
2018-06-16 19:45:47 +0100

    updated C

--- myversion_b.py
+++ myversion_b.py
@@ -1,7 +1,4 @@
 """
-expr   : term ((PLUS | MINUS) term)*
-term   : factor ((MUL | DIV) factor)*
-factor : INTEGER | LPAREN expr RPAREN
 """
 
 INT = 'INT'
@@ -18,22 +15,22 @@ import pytest
 
 
 class Token:
-	def __init__(self, typ, value):
-		self.typ = typ
-		self.value = value
+    def __init__(self, typ, value):
+        self.typ = typ
+        self.value = value
 
 
-def get_integer(text, pos):
-	result = ''
-	counter = pos
+# def get_integer(text, pos):
+#   result = ''
+#   counter = pos
 
-	while text[counter] in '0123456789':
-		result += text[counter]
-		if counter == len(text)-1:
-			break
-		counter += 1
+#   while text[counter] in '0123456789':
+#       result += text[counter]
+#       if counter == len(text)-1:
+#           break
+#       counter += 1
 
-	return Token(INT, int(result)), counter
+#   return Token(INT, int(result)), counter
 
 
 def skip_space(text, pos):
@@ -44,62 +41,63 @@ def skip_space(text, pos):
 
 
 
-def expr(text):
+
+
+def get_next_token(source, pos):
+    return pos
+
+
+def calculator(source):
+    """
+    expr   : term ((PLUS | MINUS) term)*
+    term   : factor ((MUL | DIV) factor)*
+    factor : INTEGER | LPAREN expr RPAREN
+    """
     pos = 0
-    pos = skip_space(text=text, pos=pos)
-    token, pos = get_integer(text=text, pos=pos)
-    result = token.value
-
-    while pos < len(text)-1:
-        pos = skip_space(text=text, pos=pos)
-        op = text[pos]
-        pos += 1
-        pos = skip_space(text=text, pos=pos)
-        token, pos = get_integer(text=text, pos=pos)
-
-        if op == '+':
-            result += token.value
-        elif op == '-':
-            result -= token.value
-
-    return result
+    result = get_next_token(source=source, pos=pos)
+    return ''
 
 
 
+# def expr(text):
+#     pos = 0
+#     pos = skip_space(text=text, pos=pos)
+#     token, pos = get_integer(text=text, pos=pos)
+#     result = token.value
 
-@pytest.mark.parametrize('source, expected', (
-    ('1+1', 1+1),
-    ('11+1', 11+1),
-    ('123+654', 123+654),
-    ('123  +654', 123+654),
-    ('123  + 654', 123+654),
-    ('     123  + 654', 123+654),
-    ('1+1+1', 1+1+1),
-    ('1+1+1+1+1', 1+1+1+1+1),
-    ('1  +  1   + 1  +  1  +  1', 1+1+1+1+1),
-    ('123  +  3331   + 133333  +  111  +  121', 123+3331+133333+111+121),
-    ('123  -  3331   + 133333  +  111  -  121', 123-3331+133333+111-121),
+#     while pos < len(text)-1:
+#         pos = skip_space(text=text, pos=pos)
+#         op = text[pos]
+#         pos += 1
+#         pos = skip_space(text=text, pos=pos)
+#         token, pos = get_integer(text=text, pos=pos)
+
+#         if op == '+':
+#             result += token.value
+#         elif op == '-':
+#             result -= token.value
+
+#     return result
+
+
+@pytest.mark.parametrize('kwargs, expected', (
+    ({'text': 'a', 'pos': 0}, 0),
+    ({'text': ' a', 'pos': 0}, 1),
+    ({'text': '   a', 'pos': 0}, 3),
+    ({'text': '   a', 'pos': 2}, 3),
+    ({'text': 's   a', 'pos': 2}, 4),
     ))
-def test_expr_01(source, expected):
-    assert expr(text=source) == expected
-    
-
-
-
-
+def test_skip_space(kwargs, expected):
+    pos = skip_space(**kwargs)
+    assert pos == expected
 
 
 @pytest.mark.parametrize('text, expected', (
-    ('1', ('INT', 1)),
-    ('11', ('INT', 11)),
-    ('11 ', ('INT', 11)),
-    ('11 1', ('INT', 11)),
-    ('2345', ('INT', 2345)),
+    ('', ''),
     ))
-def test_get_next_token_returns_tokens_from_pos(text, expected):
-    token = get_integer(text=text, pos=0)[0]
-    assert token.typ == Token(*expected).typ
-    assert token.value == Token(*expected).value
+def test_calculator(text, expected):
+    assert calculator(source=text) == expected
+
 
 
 if __name__ == '__main__':
@@ -107,6 +105,4 @@ if __name__ == '__main__':
 
     pytest.main([
         __file__,
-        # '-v',
-        '-s'
     ])

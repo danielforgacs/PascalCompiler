1880b8afba8b18040aa33049868f8ca38f017b71
ford@AcerS1 <@>
2019-04-30 06:15:35 +0100

    first stage

---
+++ interpreter.py
@@ -0,0 +1,66 @@
+# Token types:
+INTEGER = 'INTEGER'
+PLUS = 'PLUS'
+EOF = 'EOF'
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type_ = type_
+        # None, 0123456789, '+'
+        self.value = value
+
+
+
+class Interpreter:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+
+    def get_next_token(self):
+        text = self.text
+
+        if self.pos > len(text)-1:
+            return Token(EOF, None)
+
+        char = text[self.pos]
+
+        if char.isdigit():
+            token = Token(INTEGER, int(char))
+            self.pos += 1
+
+            return token
+
+        if char == '+':
+            token = Token(PLUS, '+')
+            self.pos += 1
+
+            return token
+
+        raise Exception('CAN`T GET TOKEN')
+
+
+    def eat(self, token_type):
+        if self.current_token.type_ == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            raise Exception('CAN NOT EAT')
+
+
+    def exp(self):
+        self.current_token = self.get_next_token()
+        left = self.current_token
+        self.eat(INTEGER)
+        op = self.current_token
+        self.eat(PLUS)
+        right = self.current_token
+        self.eat(INTEGER)
+        result = left.value + right.value
+        return result
+
+
+src = '3+5'
+interp = Interpreter(text=src)
+result  = interp.exp()
+assert result == 3+5
--- interpreter_test.py
+++ interpreter_test.py
@@ -1,1 +1,10 @@
 import interpreter
+import pytest
+
+
+@pytest.mark.parametrize('src, expected', [
+    ['3+5', 3+5],
+])
+def test_calculator_01(src, expected):
+    result = interpreter.Interpreter(src).exp()
+    assert result == expected

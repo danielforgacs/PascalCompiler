683210b143417d44d8a00452d096ac6653902a98
ford@AcerS1 <@>
2019-04-30 22:17:47 +0100

    added test

--- interpreter_test.py
+++ interpreter_test.py
@@ -12,6 +12,17 @@ def test_calculator_can_add_single_digits_without_space(src, expected):
     assert result == expected
 
 
+@pytest.mark.parametrize('src, expected', [
+    ['3-5', 3-5],
+    ['0-0', 0],
+    ['9-9', 9-9],
+    ['5-3', 5-3],
+])
+def test_calculator_can_subtract_single_digits_without_space(src, expected):
+    result = interpreter.Interpreter(src).exp()
+    assert result == expected
+
+
 def test_get_next_token():
     interp = interpreter.Interpreter(text='3+5')
     assert interp.get_next_token() == interpreter.Token(interpreter.INTEGER, 3)

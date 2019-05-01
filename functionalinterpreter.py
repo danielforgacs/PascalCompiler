8470b874f64acce3d848d98c841205200ad2d9e1
danf@VBox-Ubu19.04 <daniel.forgacs@burrows.yr.com>
2019-05-01 14:30:31 +0100

    started functional interpreter

---
+++ functionalinterpreter.py
@@ -0,0 +1,56 @@
+INTEGER = 'INTEGER'
+PLUS = 'PLUS'
+EOF = 'EOF'
+
+
+class Token:
+    def __init__(self, type_, value):
+        self.type_ = type_
+        self.value = value
+
+
+def get_next_token(src, idx):
+    char = src[idx]
+
+    if not char:
+        return Token(EOF, EOF)
+
+    elif char in '0123456789':
+        return Token(INTEGER, int(char))
+
+    elif char in '+':
+        return Token(PLUS, PLUS)
+
+
+
+def exp(src, idx=0):
+    token = get_next_token(src=src, idx=idx)
+
+    if not token.type_ == INTEGER:
+        raise
+
+    left = token
+    idx += 1
+    token = get_next_token(src=src, idx=idx)
+
+    if not token.type_ == PLUS:
+        raise
+
+    operator = token
+    idx += 1
+    token = get_next_token(src=src, idx=idx)
+
+    if not token.type_ == INTEGER:
+        raise
+
+    right = token
+
+    result = left.value + right.value
+
+    return result
+
+
+if __name__ == '__main__':
+    pass
+
+    print exp(src='3+5')

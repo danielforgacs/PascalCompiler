e8e64c01ed19bb89b1ad277e5a943ddd7124ac68
ford@AcerS1 <@>
2019-05-04 13:55:42 +0100

    wip

--- interpreter_test.py
+++ interpreter_test.py
@@ -27,7 +27,7 @@ def test_calculator_can_subtract_single_digits_without_space(src, expected):
 
 
 def test_get_next_token_add():
-    interp = interpreter.Interpreter(text='3+5')
+    interp = interpreter.Lexer(text='3+5')
     assert interp.get_next_token() == interpreter.Token(interpreter.INTEGER, 3)
     assert interp.get_next_token() == interpreter.Token(interpreter.PLUS, '+')
     assert interp.get_next_token() == interpreter.Token(interpreter.INTEGER, 5)
@@ -45,7 +45,7 @@ def test_get_next_token_add():
 
 
 def test_get_next_token_subtract():
-    interp = interpreter.Interpreter(text='3-5')
+    interp = interpreter.Lexer(text='3-5')
     assert interp.get_next_token() == interpreter.Token(interpreter.INTEGER, 3)
     assert interp.get_next_token() == interpreter.Token(interpreter.MINUS, '-')
     assert interp.get_next_token() == interpreter.Token(interpreter.INTEGER, 5)

c272a9b02a80f1e2303101c834cf6b646fc27420
ford@acer1 <-@->
2019-05-07 12:23:52 +0100

    clean up

--- functionalinterpreter.py
+++ functionalinterpreter.py
@@ -21,8 +21,10 @@ class Token:
     def __init__(self, type_, value):
         self.type_ = type_
         self.value = value
+
     def __repr__(self):
         return '<%s:%s>' % (self.type_, self.value)
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
@@ -34,8 +36,10 @@ def find_integer(src, idx):
     while char in DIGITS:
         result_char += char
         idx += 1
+
         if idx == len(src):
             break
+
         char = src[idx]
 
     integer = int(result_char)
@@ -47,17 +51,17 @@ def skip_whitespace(src, idx):
     while True:
         if idx == len(src):
             break
+
         if src[idx] == ' ':
             idx += 1
+
         else:
             break
+
     return src, idx
 
 
 def find_token(src, idx):
-    if idx == len(src):
-        return Token(EOF, EOF), idx
-
     src, idx = skip_whitespace(src, idx)
 
     if idx == len(src):
@@ -66,12 +70,15 @@ def find_token(src, idx):
     if src[idx] in DIGITS:
         number, idx = find_integer(src, idx)
         token = Token(INTEGER, number)
+
     elif src[idx] == '+':
         token = Token(PLUS, '+')
         idx += 1
+
     elif src[idx] == '-':
         token = Token(MINUS, '-')
         idx += 1
+
     else:
         raise Exception('UNEXPECTED CHARACTER')
 
@@ -80,14 +87,17 @@ def find_token(src, idx):
 
 def eat(type_, src, idx):
     token, idx = find_token(src, idx)
+
     if token.type_ == type_:
         return token.value, idx
+
     else:
         raise Exception('UNEXPECTED TOKEN')
 
 
 def factor(src, idx):
     value, idx = eat(INTEGER, src, idx)
+
     return value, idx
 
 
@@ -107,10 +117,10 @@ def expr(src, idx=0):
         elif token.type_ == PLUS:
             result, idx = factor(src, idx)
             value += result
+
         elif token.type_ == MINUS:
             result, idx = factor(src, idx)
             value -= result
-
 
     return value, idx
 

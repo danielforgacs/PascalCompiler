f054c0bb788eab1cba0028057e31d802e120cab7
ford@acer1 <-@->
2019-05-08 11:41:27 +0100

    test

--- functionalinterpreter_test.py
+++ functionalinterpreter_test.py
@@ -4,28 +4,25 @@ import functionalinterpreter as fi
 
 
 def test_find_token_tokenizes_source():
-    src = '123 456   98765 +'
-    idxs = iter([3, 7, 15, 17, 17])
-    values = iter([123, 456, 98765, '+', 'EOF'])
+    src = '123 456   98765 ++'
+    idxs = [3, 7, 15, 17, 18, 18]
+    values = iter([123, 456, 98765, '+', '+', 'EOF'])
     tokentypes = iter([
         fi.INTEGER,
         fi.INTEGER,
         fi.INTEGER,
         fi.PLUS,
+        fi.PLUS,
         fi.EOF,
     ])
 
     result = fi.find_token(src, 0)
-    assert result == (fi.Token(next(tokentypes), next(values)), next(idxs))
-    result = fi.find_token(src, result[1])
-    assert result == (fi.Token(next(tokentypes), next(values)), next(idxs))
-    result = fi.find_token(src, result[1])
-    assert result == (fi.Token(next(tokentypes), next(values)), next(idxs))
-    result = fi.find_token(src, result[1])
-    assert result == (fi.Token(next(tokentypes), next(values)), next(idxs))
-    result = fi.find_token(src, result[1])
-    assert result == (fi.Token(next(tokentypes), next(values)), next(idxs))
 
+    for idx in idxs:
+        assert result == (fi.Token(next(tokentypes), next(values)), result[1])
+        result = fi.find_token(src, idx)
+
+    assert result == (fi.Token(fi.EOF, fi.EOF), result[1])
 
 
 

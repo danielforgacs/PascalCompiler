b80cf6ba78f91fb98c4539232eef66ef6db7ddb9
ford@acer1 <-@->
2019-05-23 22:32:40 +0100

    updated grammar

--- functionalinterpreter.py
+++ functionalinterpreter.py
@@ -1,8 +1,18 @@
 """
-expr: term ((PLUS|MINUS) term)*
-term: factor ((MULT|DIV) factor)*
-factor: INTEGER | (PLUS|MINUS) factor | PAREN_LEFT expr PAREN_RIGHT
-
+program:                compound_statement DOT
+compound_statement:     BEGIN statement_list END
+statement_list:         statement | statement SEMI statement_list
+statement:              compound_statement | assignment_statement | empty
+assignment_statement:   variable ASSIGN expr
+empty:
+expr:                   term ((PLUS | MINUS) term)*
+term:                   factor ((MUL | DIV) factor)*
+factor:                 PLUS factor
+                      | MINUS factor
+                      | INTEGER
+                      | LPAREN expr RPAREN
+                      | variable
+variable:               ID
 
 -----------------------------
 plus: '+'

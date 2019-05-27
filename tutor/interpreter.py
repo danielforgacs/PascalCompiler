5f935d5b85f9f999766ae61bdcd4e6dc4c2d10a7
ford@acer1 <-@->
2019-05-27 01:13:34 +0100

    added source code to tutorial code

--- tutor/interpreter.py
+++ tutor/interpreter.py
@@ -467,7 +467,17 @@ class Interpreter(NodeVisitor):
 
 def main():
     import sys
-    text = open(sys.argv[1], 'r').read()
+    text = """
+BEGIN
+    BEGIN
+        number := 2;
+        a := number;
+        b := 10 * a + 10 * number / 4;
+        c := a - - b
+    END;
+    x := 11;
+END.
+"""
 
     lexer = Lexer(text)
     parser = Parser(lexer)
